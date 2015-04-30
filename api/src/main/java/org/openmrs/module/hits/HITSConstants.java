664e96f7ea59120f7b1d76a20971f24791a5db87
geoff-wasilwa <geoffreywanjala@gmail.com>
2015-04-30 15:31:55 +0300

    Handle connection issues - enhancement
    Added method to handle 404 and 408 (Client timeouts) connection errors by saving values to file system.

---
+++ api/src/main/java/org/openmrs/module/hits/HITSConstants.java
@@ -0,0 +1,12 @@
+package org.openmrs.module.hits;
+
+
+public class HITSConstants {
+	
+	public static final String MOTHER_ID = "mother_id";
+	
+	public static final String HEI_ID = "infant_ID";
+	
+	public static final String PROPERTIES_FILE_LOCATION_GLOBAL_PROPERTY = "hits.propertiesFileLocation";
+	
+}
