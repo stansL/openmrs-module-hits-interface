9918dc6117bf3d84cfa109c37488388c23da1ad6
Ken Omondi Ochieng <omondi.ken@gmail.com>
2015-05-03 13:09:26 +0300

    Routine Maintenance
    Explicitly defined the hits id and hei id as constants

--- api/src/main/java/org/openmrs/module/hits/mapping/PatientDetailsMapping.java
+++ api/src/main/java/org/openmrs/module/hits/mapping/PatientDetailsMapping.java
@@ -1,6 +1,5 @@
 package org.openmrs.module.hits.mapping;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import org.joda.time.DateTime;
@@ -10,50 +9,39 @@ import org.openmrs.Relationship;
 import org.openmrs.api.context.Context;
 
 public class PatientDetailsMapping {
-	
+
+	private static final int HEI_ID = 8;
+	private static final int HITS_ID = 9;
 	private Patient patient;
-	
+
 	public Patient getPatient() {
 		return patient;
 	}
-	
+
 	public void setPatient(Patient patient) {
 		this.patient = patient;
 	}
-	
-	public PatientDetailsMapping(Patient patient)
-	{
-		this.patient = patient;		
+
+	public PatientDetailsMapping(Patient patient) {
+		this.patient = patient;
 	}
-	
-	public void mapPatientDetails(Map<String,String> values)
-	{
-		PatientIdentifier hitsIdentifier = patient
-				.getPatientIdentifier(
-						Context.getPatientService()
-								.getPatientIdentifierType(9));
-		PatientIdentifier heiIdentifier = patient
-				.getPatientIdentifier(
-						Context.getPatientService()
-								.getPatientIdentifierType(8));
-		
-		String formattedDob = new DateTime(patient
-				.getBirthdate()).toString("yyyy/MM/dd");
-			
+
+	public void mapPatientDetails(Map<String, String> values) {
+		PatientIdentifier hitsIdentifier = patient.getPatientIdentifier(Context.getPatientService().getPatientIdentifierType(HITS_ID));
+		PatientIdentifier heiIdentifier = patient.getPatientIdentifier(Context.getPatientService().getPatientIdentifierType(HEI_ID));
+
+		String formattedDob = new DateTime(patient.getBirthdate()).toString("yyyy/MM/dd");
+
 		Patient mother = null;
-		for (Relationship relationship : Context.getPersonService()
-				.getRelationshipsByPerson(patient)) {
-			if (relationship.getRelationshipType().getbIsToA()
-					.equals("Child")) {
+		for (Relationship relationship : Context.getPersonService().getRelationshipsByPerson(patient)) {
+			if (relationship.getRelationshipType().getbIsToA().equals("Child")) {
 				if (relationship.getPersonA().getGender().equals("F")) {
-					mother = Context.getPatientService().getPatient(
-							relationship.getPersonA().getId());
+					mother = Context.getPatientService().getPatient(relationship.getPersonA().getId());
 				}
 			}
-	
+
 		}
-	
-		
+
 		if (hitsIdentifier != null) {
 			values.put("hitsid", hitsIdentifier.getIdentifier());
 		}
@@ -62,13 +50,11 @@ public class PatientDetailsMapping {
 		}
 		values.put("infant_dob", formattedDob);
 		values.put("infant_gender", patient.getGender());
-	
+
 		if (mother != null) {
-			values.put("mother_dob", new DateTime(mother.getBirthdate())
-					.toString("yyyy/MM/dd"));
+			values.put("mother_dob", new DateTime(mother.getBirthdate()).toString("yyyy/MM/dd"));
 			values.put("mother_age", mother.getAge().toString());
-			values.put("mother_id", mother.getPatientIdentifier()
-					.getIdentifier());
+			values.put("mother_id", mother.getPatientIdentifier().getIdentifier());
 		}
 	}
 }
